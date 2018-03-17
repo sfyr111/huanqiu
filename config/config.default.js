54c94a92e663c795fae436230b7427ef7d6e2b1b
sfyr111 <sfyr111@163.com>
2018-03-18 00:02:13 +0800

    done config

--- config/config.default.js
+++ config/config.default.js
@@ -1,13 +1,33 @@
 'use strict';
 
+const _config = require('../config');
+
 module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1521294048471_7399';
+  config.keys = appInfo.name + '_HUAN_QIU';
 
   // add your config here
   config.middleware = [];
+
+  config.sequelize = _config.mysql;
+
+  config.redis = _config.redis;
+
+  config.sessionRedis = {
+    key: 'huanqiu',
+    maxAge: 24 * 3600 * 1000,
+    httpOnly: true,
+    encrypt: false,
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+
 
   return config;
 };
--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,23 @@
 
 // had enabled by egg
 // exports.static = true;
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
+};
+
+exports.redis = {
+  enable: true,
+  package: 'egg-redis',
+};
+
+exports.sessionRedis = {
+  enable: true,
+  package: 'egg-session-redis',
+};
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
+};
